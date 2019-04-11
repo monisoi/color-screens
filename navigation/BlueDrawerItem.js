713bd1ca13c9f9f696f3d6ae39e6ded4a83974ac
monisoi <ichii.nakamura@gmail.com>
2019-04-12 08:48:55 +0900

    switch to the initial screen

--- navigation/BlueDrawerItem.js
+++ navigation/BlueDrawerItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, StackActions, NavigationActions } from 'react-navigation';
 
 import BlueScreen1 from '../screens/BlueScreen1';
 import BlueScreen2 from '../screens/BlueScreen2';
@@ -11,25 +11,40 @@ import BlueScreen6 from '../screens/BlueScreen6';
 import BlueScreen7 from '../screens/BlueScreen7';
 import BlueScreen8 from '../screens/BlueScreen8';
 
+const stackScreens = {
+  Blue1: BlueScreen1,
+  Blue2: BlueScreen2,
+  Blue3: BlueScreen3,
+  Blue4: BlueScreen4,
+  Blue5: BlueScreen5,
+  Blue6: BlueScreen6,
+  Blue7: BlueScreen7,
+  Blue8: BlueScreen8,
+};
+
 export const BlueStack = createStackNavigator(
-  {
-    Blue1: BlueScreen1,
-    Blue2: BlueScreen2,
-    Blue3: BlueScreen3,
-    Blue4: BlueScreen4,
-    Blue5: BlueScreen5,
-    Blue6: BlueScreen6,
-    Blue7: BlueScreen7,
-    Blue8: BlueScreen8,
-  },
+  stackScreens,
   {
     initialRouteName: 'Blue1',
   }
 );
 
+const resetAction = StackActions.reset({
+  index: 0,
+  key: null,
+  actions: [
+    NavigationActions.navigate({ routeName: 'Blue1' }),
+  ],
+});
+
 export const BlueDrawerItem = props => {
   return (
-    <TouchableOpacity onPress={() => props.navigation.navigate('Blue1')} style={styles.drawerItem}>
+    <TouchableOpacity
+      onPress={() => Object.keys(stackRouters).includes(props.navigation.state.routeName) ?
+        resetAction :
+        props.navigation.navigate('Blue1')}
+      style={styles.drawerItem}
+    >
       <Text style={styles.title}>Blue</Text>
     </TouchableOpacity>
   );
--- navigation/RedDrawerItem.js
+++ navigation/RedDrawerItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, StackActions, NavigationActions } from 'react-navigation';
 
 import RedScreen1 from '../screens/RedScreen1';
 import RedScreen2 from '../screens/RedScreen2';
@@ -11,8 +11,7 @@ import RedScreen6 from '../screens/RedScreen6';
 import RedScreen7 from '../screens/RedScreen7';
 import RedScreen8 from '../screens/RedScreen8';
 
-export const RedStack = createStackNavigator(
-  {
+const stackScreens = {
     Red1: RedScreen1,
     Red2: RedScreen2,
     Red3: RedScreen3,
@@ -21,15 +20,31 @@ export const RedStack = createStackNavigator(
     Red6: RedScreen6,
     Red7: RedScreen7,
     Red8: RedScreen8,
-  },
+};
+
+export const RedStack = createStackNavigator(
+  stackScreens,
   {
     initialRouteName: 'Red1',
   }
 );
 
+const resetAction = StackActions.reset({
+  index: 0,
+  key: null,
+  actions: [
+    NavigationActions.navigate({ routeName: 'Red1' }),
+  ],
+});
+
 export const RedDrawerItem = props => {
   return (
-    <TouchableOpacity onPress={() => props.navigation.navigate('Red1')} style={styles.drawerItem}>
+    <TouchableOpacity
+      onPress={() => Object.keys(stackRouters).includes(props.navigation.state.routeName) ?
+        resetAction :
+        props.navigation.navigate('Red1')}
+      style={styles.drawerItem}
+    >
       <Text style={styles.title}>Red</Text>
     </TouchableOpacity>
   );
